eac704698b36833870da17bf12d79d6e0cacf499
Naoya0523 <nintendonaoya@gmail.com>
2023-04-11 18:57:53 +0900

    add function

--- selection.py
+++ selection.py
@@ -2,23 +2,52 @@ import cv2
 import pandas as pd
 import os
 
-filepath = "test-csv.csv"
+filepath = ""
 df = pd.read_csv(filepath)
-
-annotation_csv = pd.DataFrame()
 
 try:
     os.mkdir("Transformed-Images")
 except Exception:
     pass
 
-img_folder_path = "Images/"
-for row in df.itertuples():
-    img_path = img_folder_path + row[1]
-    xmin = row[2]
-    ymin = row[3]
-    xmax = row[4]
-    ymax = row[5]
-    tag = row[6]
+img_folder_path = ""
 
-print(img_path, xmin, ymin, xmax, ymax)
+def get_img_and_annotations(df):
+    num = 0
+    list_for_csv = []
+
+    print("processing...")
+
+    for row in df.itertuples():
+        value_list = []
+        img_path = img_folder_path + row[1]
+
+        #coordinate
+        xmin = int(row[2])
+        ymin = int(row[3])
+        xmax = int(row[4])
+        ymax = int(row[5])
+
+        #label
+        label = row[6]
+
+        #save the image
+        img_name = "img" + str(num) + ".jpg"
+
+        img = cv2.imread(img_path)
+        target_img = img[ymin:ymax, xmin:xmax]
+        cv2.imwrite("Transformed-Images/" + img_name, target_img)
+
+        #make a list for csv file
+        value_list.append(img_name)
+        value_list.append(label)
+        list_for_csv.append(value_list)
+
+        num += 1
+
+    annotation_csv = pd.DataFrame(list_for_csv, columns=["img", "tag"])
+    annotation_csv.to_csv("annotations.csv")
+    print("completed!")
+
+if __name__ == "__main__":
+    get_img_and_annotations(df)
